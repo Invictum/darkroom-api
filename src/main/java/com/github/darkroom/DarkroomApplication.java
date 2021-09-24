65d9db3588a6771eea8bc83d538c4acaff30ffe8
Iaroslav Iershov <zim182@gmail.com>
2021-09-23 20:42:18 -0500

    Initial commit

---
+++ src/main/java/com/github/darkroom/DarkroomApplication.java
@@ -0,0 +1,12 @@
+package com.github.darkroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DarkroomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DarkroomApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/darkroom/DarkroomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.darkroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DarkroomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
